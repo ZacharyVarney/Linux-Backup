260f5690feba02d433177a6b522f5d0719acfbb9
Zachary Varney <56404614+ZacharyVarney@users.noreply.github.com>
2020-11-25 23:03:35 -0500

    Update programs-install.sh

--- Scripts/programs-install.sh
+++ Scripts/programs-install.sh
@@ -4,7 +4,7 @@ sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 
 sudo pacman -Syy
 
-sudo pacman -S nvidia-settings grub-customizer flatpak teamspeak3 discord code steam
+sudo pacman -S nvidia-settings grub-customizer flatpak teamspeak3 discord code steam rofi terminator
 
 sudo flatpak install spotify
 
