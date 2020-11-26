5c4a41b88371269dfc28db90cc23bb779068c5cb
Zachary Varney <56404614+ZacharyVarney@users.noreply.github.com>
2020-11-25 22:59:43 -0500

    Add files via upload

---
+++ Scripts/programs-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash/
+
+sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
+
+sudo pacman -Syy
+
+sudo pacman -S nvidia-settings grub-customizer flatpak teamspeak3 discord code steam
+
+sudo flatpak install spotify
+
+trizen -S brave-bin timeshift
