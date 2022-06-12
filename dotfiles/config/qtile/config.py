982cc51ada89cad7254af4034a39db5c4a6be5e6
Zachary Varney <56404614+ZacharyVarney@users.noreply.github.com>
2022-06-12 13:36:35 +0000

    Add files via upload

--- dotfiles/config/qtile/config.py
+++ dotfiles/config/qtile/config.py
@@ -1,5 +1,3 @@
-from typing import List  # noqa: F401
-
 from libqtile import bar, layout, widget
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
@@ -18,71 +16,56 @@ mod2 = "mod1"
 terminal = "alacritty"
 
 keys = [
+    # A list of available commands that can be bound to keys can be found
+    # at https://docs.qtile.org/en/latest/manual/config/lazy.html
     # Switch between windows
     Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(),
-        desc="Move window focus to other window"),
-
+    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+    
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(),
-        desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(),
-        desc="Move window to the right"),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down(),
-        desc="Move window down"),
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-
+    
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(),
-        desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(),
-        desc="Grow window to the right"),
-    Key([mod, "control"], "j", lazy.layout.grow_down(),
-        desc="Grow window down"),
+    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
+    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    
+    # Qtile keys
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
-    Key([mod, "shift"], "Return", lazy.layout.toggle_split(),
-        desc="Toggle between split and unsplit sides of stack"),
     Key([mod], "t", lazy.spawn(terminal), desc="Launch terminal"),
-
-    # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([mod], "m", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "c", lazy.window.kill(), desc="Kill focused window"),
-
-	# Application shortcuts
-	Key([mod], "r", lazy.spawn("rofi -modi drun,run -show drun -show-icons"), desc="Run prompt"),
+    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
+    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+    
+    # App binds
+	Key([mod], "r", lazy.spawn("bash /home/zach/.config/rofi/launchers/misc/launcher.sh"), desc="Run prompt"),
 	Key([mod], "b", lazy.spawn("brave"), desc="Run browser"),
 	Key([mod], "f", lazy.spawn("pcmanfm"), desc="Run file manager"),
-	Key([mod], "v", lazy.spawn("gnome-system-monitor"), desc="Run system monitor"),
+	Key([mod], "v", lazy.spawn("xfce4-taskmanager"), desc="Run system monitor"),
 	Key([mod], "p", lazy.spawn("pavucontrol"), desc="Run sound mixer"),
-	Key([mod], "Print", lazy.spawn("flameshot screen -p /home/zach/Pictures/Screenshots"), desc="Take screenshot"),
-	Key([mod], "u", lazy.spawn("betterlockscreen -l blur --time-format \"%l:%M %p\""), desc="Lockscreen"),
+	Key([mod], "F8", lazy.spawn("flameshot gui"), desc="Take screenshot"),
 	Key([mod], "o", lazy.spawn("killall picom"), desc="Kill compositor"),
-	Key([mod], "i", lazy.spawn("picom --experimental-backend"), desc="Start compositor"),
-	Key([mod2], "Tab", lazy.spawn("rofi -modi window -show window -show-icons"), desc="Show open window list"),
-
-	# Qtile
-    Key([mod, "control"], "r", lazy.restart(), desc="Restart Qtile"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+	Key([mod], "i", lazy.spawn("picom --experimental-backends"), desc="Start compositor"),
+	Key([mod2], "Tab", lazy.spawn("bash /home/zach/.config/rofi/launchers/misc/launcher-window.sh"), desc="Show open window list"),
 ]
 
 def init_group_names():
-	return [("  ", {'layout': 'max'}),
-			("  ", {'layout': 'max'}),
-			("  ", {'layout': 'columns'}),
-			("  ", {'layout': 'columns'}),
-			("  ", {'layout': 'max'}),
-			("  ", {'layout': 'columns'})]
+	return [("  ", {'layout': 'monadtall'}),
+			("  ", {'layout': 'max'}),
+			("  ", {'layout': 'monadtall'}),
+			("  ", {'layout': 'monadtall'}),
+			("  ", {'layout': 'max'}),
+			("  ", {'layout': 'monadtall'})]
 
 def init_groups():
 	return [Group(name, **kwargs) for name, kwargs in group_names]
@@ -97,36 +80,29 @@ for i, (name, kwargs) in enumerate(group_names, 1):
 	keys.append(Key([mod, "shift"], str(i), lazy.window.togroup(name, switch_group=True)))
 
 layouts = [
-	layout.Tile(
+	layout.MonadTall(
 		margin = 20,
-		border_focus = '#0D6DE8',
+		border_focus = '#3F51B5',
 		border_normal = '#000000'
 	),
-	layout.RatioTile(
-		margin = 20,
-		border_focus = '#0D6DE8',
-		border_normal = '#000000'
-	),
-    layout.Columns(
-		margin = 15,
-		border_focus = '#0D6DE8',
-		border_normal = '#000000'
-    ),
     layout.Max(),
     # Try more layouts by unleashing below layouts.
+    # layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
     # layout.Stack(num_stacks=2),
     # layout.Bsp(),
     # layout.Matrix(),
-    # layout.MonadTall(),
     # layout.MonadWide(),
+    # layout.RatioTile(),
+    # layout.Tile(),
     # layout.TreeTab(),
     # layout.VerticalTile(),
     # layout.Zoomy(),
 ]
 
 widget_defaults = dict(
-    font='SFMono Nerd Font Mono',
-    fontsize=13,
+    font="Roboto Medium",
+    fontsize=12,
+    padding=3,
 )
 extension_defaults = widget_defaults.copy()
 
@@ -141,101 +117,110 @@ screens = [
 					rounded = False,
 					padding = 9,
 					margin_x = 0,
-					other_current_screen_border = '#0D6DE8',
-					other_screen_border = '#1E1E1E',
-					this_current_screen_border = '0D6DE8',
-					this_screen_border = '#1E1E1E',
+					other_current_screen_border = '#3F51B5',
+					other_screen_border = '#303030',
+					this_current_screen_border = '#3F51B5',
+					this_screen_border = '#303030',
 					urgent_alert_method = 'block',
-					fontsize = 26
+					urgent_text = '#EE4900',
+					spacing = .5,
+					font = 'FantasqueSansMono Nerd Font Regular',
+					fontsize = 28
                 ),
                 widget.WindowName(
 					empty_group_string = 'Arch Linux - Qtile',
-					max_chars = 45,
-					padding = 15,
+					max_chars = 60,
+					padding = 10,
 					foreground = '#ffffff',
-					font = 'Fira Sans Condensed SemiBold'
+					format = '{name}',
+					for_current_screen = True
                 ),
-                #widget.OpenWeather(
-				#	app_key = 'ade9990c9f83722d69a21e49432916d1',
-				#	cityid = 4303543,
-				#	font = 'Fira Sans Condensed SemiBold',
-				#	fontsize = 16,
-				#	metric = False,
-				#	padding = 15,
-				#	update_interval = 1200,
-				#	format = ' {main_temp} °{units_temperature} {weather_details}'
-                #),
+                widget.TextBox(
+					fmt = ' ',
+					font = 'FantasqueSansMono Nerd Font Regular',
+					fontsize = 11
+                ),
                 widget.Memory(
-					padding = 15,
-					format = ' {MemUsed: .0f}{mm}',
-					font = 'Fira Sans Condensed SemiBold',
-					fontsize = 16,
+					format = '{MemUsed: .0f}{mm}',
 					measure_mem = 'G'
                 ),
+                widget.Sep(
+					linewidth = 0,
+					padding = 20
+                ),
+                widget.TextBox(
+					fmt = ' ',
+					font = 'FantasqueSansMono Nerd Font Regular',
+					fontsize = 11
+                ),
                 widget.CPU(
-					padding = 15,
-					format = ' {load_percent}%',
-					font = 'Fira Sans Condensed SemiBold',
-					fontsize = 16
+					format = '{load_percent}%'
+                ),
+                widget.Sep(
+					linewidth = 0,
+					padding = 20
+                ),
+                widget.TextBox(
+					fmt = ' ',
+					font = 'FantasqueSansMono Nerd Font Regular',
+					fontsize = 11
                 ),
                 widget.CheckUpdates(
 					distro = 'Arch_checkupdates',
-					display_format = ' {updates}',
-					padding = 15,
-					execute = 'terminator -e paru -Syu',
-					no_update_string = ' 0',
-					fontsize = 16,
-					font = 'Fira Sans Condensed SemiBold'
+					display_format = '{updates}',
+					execute = 'alacritty -e paru -Syu',
+					no_update_string = '0'
                 ),
                 widget.Clock(
 					format='%a %b %d, %l:%M %p',
-					padding = 15,
-					font = 'Fira Sans Condensed SemiBold'
+					padding = 20
                 ),
                 widget.Systray(
-					icon_size = 18
+					icon_size = 17,
+					padding = 9
+                ),
+                widget.Sep(
+					linewidth = 0,
+					padding = 20
                 ),
             ],
             34,
-            background = '#121212',
-            margin=[0, 0, 0, 0],
-            opacity=0.8,
+            background = '#111111',
+            margin = [0, 0, 0, 0],
+            opacity = 0.8,
         ),
     ),
 ]
 
 # Drag floating layouts.
 mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(),
-         start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(),
-         start=lazy.window.get_size()),
-    Click([mod], "Button2", lazy.window.bring_to_front())
+    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front()),
 ]
 
 dgroups_key_binder = None
-dgroups_app_rules = []  # type: List
+dgroups_app_rules = []  # type: list
 follow_mouse_focus = True
 bring_front_click = False
 cursor_warp = False
-floating_layout = layout.Floating(float_rules=[
-    # Run the utility of `xprop` to see the wm class and name of an X client.
-    *layout.Floating.default_float_rules,
-    Match(wm_class='confirmreset'),  # gitk
-    Match(wm_class='makebranch'),  # gitk
-    Match(wm_class='maketag'),  # gitk
-    Match(wm_class='ssh-askpass'),  # ssh-askpass
-    Match(wm_class='Steam'),  # steam
-    Match(title='branchdialog'),  # gitk
-    Match(title='Nitrogen'),  # nitrogen
-    Match(title='pinentry'),  # GPG key password entry
-], border_focus = '#ffffff', border_width = 1)
+floating_layout = layout.Floating(
+    float_rules=[
+        # Run the utility of `xprop` to see the wm class and name of an X client.
+        *layout.Floating.default_float_rules,
+        Match(wm_class='confirmreset'),  # gitk
+		Match(wm_class='makebranch'),  # gitk
+		Match(wm_class='maketag'),  # gitk
+		Match(wm_class='ssh-askpass'),  # ssh-askpass
+		Match(wm_class='Steam'),  # steam
+		Match(title='branchdialog'),  # gitk
+		Match(title='Nitrogen'),  # nitrogen
+		Match(title='pinentry'),  # GPG key password entry
+    ], border_focus = '#ffffff', border_width = 1)
+
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
-
-# If things like steam games want to auto-minimize themselves when losing
-# focus, should we respect this or not?
 auto_minimize = True
-
+wl_input_rules = None
 wmname = "qtile"
