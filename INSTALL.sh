0d19643cd8eb344b3add866688db6af89202762f
Zachary Varney <56404614+ZacharyVarney@users.noreply.github.com>
2022-10-18 02:37:29 -0400

    Add files via upload

--- INSTALL.sh
+++ INSTALL.sh
@@ -6,11 +6,13 @@ read -p "Do you use an NVIDIA gpu [y/n]: " ANS
 
 if [[ "$ANS" == "y" ]]; 
 then
-	sudo archinstall --config /home/zach/Desktop/nvidia_user_configuration.json
+	sudo archinstall --config /home/live/install-configs/nvidia_user_configuration.json
 else
-	sudo archinstall --config /home/zach/Desktop/user_configuration.json
+	sudo archinstall --config /home/live/install-configs/user_configuration.json
 fi
 
 echo " "
 echo " "
-echo "Base system install completed."
+echo " "
+echo "Base system installed, you may now reboot & run SETUP.sh"
+sleep 5
--- SETUP.sh
+++ SETUP.sh
@@ -2,8 +2,10 @@
 
 clear
 
-echo "In order for these commands to work properly you need to be chrooted into installed system."
-sleep 8
+echo "The commands run in this script will require root privileges"
+sleep 5
+
+cd $HOME
 
 echo " "
 echo " "
@@ -18,12 +20,27 @@ echo "Installing Paru AUR helper"
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
+cd ..
+rm -rf paru
 
 echo " "
 echo " "
-echo "Installing recommended AUR apps"
-paru -S timeshift brave-bin
+echo "Updating pacman.conf"
+sudo cp Linux-Backup/pacman.conf /etc/
+sudo pacman -Syuu
 
 echo " "
 echo " "
-echo "Post installation finished, you are now fine to reboot."
+echo "Installing recommended AUR packages"
+paru -S timeshift brave-bin mangohud lib32-mangohud goverlay-bin
+
+echo " "
+echo " "
+echo "Installing recommended flatpak packages"
+flatpak install pcsx2 dolphin-emu duckstation citra yuzu ppsspp retroarch rpcs3 pupgui2 app.xemu.xemu
+
+echo " "
+echo " "
+echo " "
+echo "Setup has finished, you can now reboot your system"
+sleep 5
