588eb6b4d6dfdb9dbde0d59ad320f2288ac089d4
Zachary Varney <56404614+ZacharyVarney@users.noreply.github.com>
2024-05-12 19:42:51 -0400

    Add files via upload

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -1,11 +1,9 @@
 if status is-interactive
-    neofetch
-
+    fastfetch
     set fish_greeting
-
     starship init fish | source
 
     alias cat="bat"
-
-    alias wine-reset="sudo bash /home/zach/.scripts/wine-reset.sh"
+    alias wine-reset="sudo bash /home/zach/Documents/wine-reset.sh"
+    alias update-grub="sudo grub-mkconfig -o /boot/grub/grub.cfg"
 end
