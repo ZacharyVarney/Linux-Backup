69cd1102acd9b2856f271a4819543207e735868e
Zachary Varney <56404614+ZacharyVarney@users.noreply.github.com>
2024-05-17 01:28:24 -0400

    Update wine-reset.sh

--- dotfiles/scripts/wine-reset.sh
+++ dotfiles/scripts/wine-reset.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# Sometimes when wine/proton apps don't shutdown gracefully they will linger open & caused unwanted behaviours
+
 PROTONCORE=(pv-bwrap pressure-vessel reaper explorer.exe rpcss.exe plugplay.exe services.exe svchost.exe winedevice.exe winedevice.exe wineserver gamescope gamescope-wl)
 
 for PROG in "${PROTONCORE[@]}"; do
