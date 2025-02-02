1553b526d3f868201dc9655d4fda4c80ffa0bcb6
Zachary Varney <56404614+ZacharyVarney@users.noreply.github.com>
2025-02-01 20:11:32 -0500

    Delete dotfiles/scripts/start-replay.sh

--- dotfiles/scripts/start-replay.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-pidof -q gpu-screen-recorder && exit 0
-video_path="$HOME/Videos"
-mkdir -p "$video_path"
-gpu-screen-recorder -w DP-1 -f 60 -a "default_output|default_input" -c mp4 -k h264 -ac opus -r 60 -fm vfr -o "$video_path"
