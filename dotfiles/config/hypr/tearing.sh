e02b143d9db3dc8e50b891dc184e36049c88f276
Zachary Varney <56404614+ZacharyVarney@users.noreply.github.com>
2025-08-16 23:31:42 -0400

    Add files via upload

---
+++ dotfiles/config/hypr/tearing.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Apply new settings
+hyprctl keyword general:allow_tearing true
+hyprctl keyword cursor:no_hardware_cursors 1
+hyprctl keyword misc:vrr 0
+notify-send "Tearing Script" "Tearing Activated, VRR Disabled"
+
+# Launch game (replace with actual launch command)
+"$@"
+
+# Restore original settings
+hyprctl keyword general:allow_tearing false
+hyprctl keyword cursor:no_hardware_cursors 0
+hyprctl keyword misc:vrr 3
+notify-send "Tearing Script" "Tearing Deactivated, VRR Enabled"
